44eae07a6fbcc2d3ccaf05d423781c21d377d0e3
rvillarb <rvillarb@emeal.nttdata.com>
2024-09-24 20:09:22 -0300

    Cambio en la validacion para ver los modulos de super admin.

--- app/Models/Perfil.php
+++ app/Models/Perfil.php
@@ -35,7 +35,7 @@ class Perfil extends Model
 
         $db = \Config\Database::connect();
         //$sql = "select * from perfil where poder < :poder: AND feliminacion IS NULL";
-        $sql = "select * from perfil where poder < :poder: ";
+        $sql = "select * from perfil where poder <= :poder: ";
         $perfil = $db->query($sql,['poder' => session()->get('usuario')['poder']])->getResult('object');
         return $perfil;   
     }
